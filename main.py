40a43664d204dc0f6dc11ab0e732cd29f61cb78d
Maicon P. B. Birolo <maicondante@hotmail.com>
2026-06-15 11:10:34 -0300

    feat: Adiciona sistema de temas com qss global.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from app.ui.main_window import MainWindow
+from PySide6.QtCore import QFile, QTextStream
+
+def load_styles(app):
+    file = QFile("app/styles/theme.qss")
+
+    if file.open(QFile.ReadOnly | QFile.Text):
+        stream = QTextStream(file)
+        app.setStyleSheet(stream.readAll())
 
 def main():
     app = QApplication(["Finance Manager"])
+    load_styles(app)
 
     window = MainWindow()
     window.show()
